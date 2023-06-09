604b6ab74d8b8eb5f80a6dca5de35c212e04a983
Veikko Lintujärvi <veikko@lintujarvi.fi>
2023-06-09 10:03:07 +0300

    Update open hours

--- components/OpenHours.tsx
+++ components/OpenHours.tsx
@@ -5,7 +5,11 @@ export const OpenHours = () => {
   return (
     <>
       <h2 className={styles.openHoursTitle}>Auki:</h2>
-      <p className={styles.openHoursText}>ma-la 12:00-19:00</p>
+      <p className={styles.openHoursText}>
+        su-to 09:00-19:00
+        <br />
+        pe-la 09:00-21:00
+      </p>
       <p className={styles.openHoursDesc}>
         kysynnän ja sään salliessa myös pidempään
       </p>
