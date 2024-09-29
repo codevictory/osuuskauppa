123db501829fb6814bd1cc73ceb676dc53c39141
Veikko Lintujärvi <veikko@lintujarvi.fi>
2024-09-29 18:24:50 +0300

    Winter open hours update

--- components/OpenHours.tsx
+++ components/OpenHours.tsx
@@ -4,11 +4,12 @@ import styles from '../styles/OpenHours.module.scss';
 export const OpenHours = () => {
   return (
     <>
-      <p className={styles.openHoursText}>su-to 11:00-19:00</p>
-      {/* Only during summer season*/}
+      <p className={styles.openHoursText}>ma-la 12:00-19:00</p>
+      {/* Only during summer season
       <p className={styles.openHoursDesc}>
         pe-la 11:00-21:00
       </p>
+      */}
       <h2 className={styles.openHoursTitle}>Tervetuloa!</h2>
     </>
   );
